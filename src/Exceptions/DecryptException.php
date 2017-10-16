45aad4d77c0660abeee6e916540ae0fbb6445ae6
siriusSupreme <guoplc@163.com>
2017-10-16 15:04:11 +0800

    ''

---
+++ src/Exceptions/DecryptException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sirius\Encrypt\Exceptions;
+
+use RuntimeException;
+
+class DecryptException extends RuntimeException
+{
+    //
+}
---
+++ src/Exceptions/EncryptException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sirius\Encrypt\Exceptions;
+
+use RuntimeException;
+
+class EncryptException extends RuntimeException
+{
+    //
+}
